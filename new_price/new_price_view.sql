5b72bb2e0fc9b97ce9eea8b6fb9877a0bd3bac87
TAR <warintorn.realist@gmail.com>
2024-02-14 15:43:27 +0700

    source_new_condo_price_calculate_view

--- new_price/new_price_view.sql
+++ new_price/new_price_view.sql
@@ -3,14 +3,15 @@
 
 -- source_all_price_view
 CREATE or replace VIEW `source_all_price_view` AS
-select * from (select Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark
+select * from (select Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Resale, Price_Source, Price_Type, Special, Remark
 				from real_condo_price_new
 				where Price_Status = '1') np
-union select * from (select Condo_Code
-						, Data_Value as Price
+union all select * from (select Condo_Code
+						, if(Data_Attribute='price_per_sqm',Data_Value,Data_Value*1000000) as Price
 						, Data_Date as Price_Date
 						, '0' as Condo_Build_Date
-						, 'เฉลี่ย' as Start_or_Average
+						, if(Data_Attribute='price_per_sqm','เฉลี่ย','เริ่มต้น') as Start_or_Average
+						, '0' as Resale
 						, 1 as Price_Source
 						, if(Data_Attribute='price_per_sqm','บ/ตรม','บ/ยูนิต') as Price_Type
 						, '0' as Special
@@ -18,11 +19,12 @@ union select * from (select Condo_Code
 					from real_condo_561
 					where Data_Status = '1'
 					and (Data_Attribute = 'price_per_sqm' or Data_Attribute = 'price_per_unit_mb')) rc561
-union select * from (select Condo_Code
+union all select * from (select Condo_Code
 						, Data_Value as Price
 						, Data_Date as Price_Date
 						, '0' as Condo_Build_Date
 						, 'เฉลี่ย' as Start_or_Average
+						, '1' as Resale
 						, 2 as Price_Source
 						, 'บ/ตรม' as Price_Type
 						, '0' as Special
@@ -33,8 +35,271 @@ union select * from (select Condo_Code
 
 
 -- source_new_condo_price_calculate_view
+create or replace view source_new_condo_price_calculate_view as
 select a.Condo_Code
+	, if(b.Condo_Built_Finished is not null
+		, if((year(curdate()) - (year(b.Condo_Built_Finished) + 1)) > 0
+			, 'OLD-finishDate'
+			, 'NEW-finishDate')
+		, if(b.Condo_Built_Start is not null
+			, if(a.Condo_HighRise = 1
+				, if((year(curdate()) - (year(b.Condo_Built_Start) + 4)) > 0
+					, 'OLD-launchDate-HighRise(4)'
+					, 'NEW-launchDate-HighRise(4)')
+				, if((year(curdate()) - (year(b.Condo_Built_Start) + 3)) > 0
+					, 'OLD-launchDate-HighRise(3)'
+					, 'NEW-launchDate-HighRise(3)'))
+			, 'OLD-donno')) as Old_or_New
+	, if(cal56_1_sqm.Price is not null or cal_hip.Price is not null
+		, 'ราคาเฉลี่ย'
+		, if(cal_bgsq.Price is not null or cal_d1sq.Price is not null
+			, 'ราคาเริ่มต้น'
+			, '')) as Condo_Age_Status_Square_Text
+	, if(if(b.Condo_Built_Finished is not null
+			, if((year(curdate()) - (year(b.Condo_Built_Finished) + 1)) > 0
+				, 'OLD'
+				, 'NEW')
+			, if(b.Condo_Built_Start is not null
+				, if(a.Condo_HighRise = 1
+					, if((year(curdate()) - (year(b.Condo_Built_Start) + 4)) > 0
+						, 'OLD'
+						, 'NEW')
+					, if((year(curdate()) - (year(b.Condo_Built_Start) + 3)) > 0
+						, 'OLD'
+						, 'NEW'))
+				, 'OLD')) = 'NEW'
+		, ifnull(cal56_1_sqm.Price
+			, ifnull(cal_hip.Price
+				, ifnull(cal_bgsq.Price
+					, cal_d1sq.Price)))
+		, ifnull(cal_hip.Price
+			, ifnull(cal56_1_sqm.Price
+				, ifnull(cal_bgsq.Price
+					, cal_d1sq.Price)))) as Condo_Price_Per_Square
+	, if(if(b.Condo_Built_Finished is not null
+			, if((year(curdate()) - (year(b.Condo_Built_Finished) + 1)) > 0
+				, 'OLD'
+				, 'NEW')
+			, if(b.Condo_Built_Start is not null
+				, if(a.Condo_HighRise = 1
+					, if((year(curdate()) - (year(b.Condo_Built_Start) + 4)) > 0
+						, 'OLD'
+						, 'NEW')
+					, if((year(curdate()) - (year(b.Condo_Built_Start) + 3)) > 0
+						, 'OLD'
+						, 'NEW'))
+				, 'OLD')) = 'NEW'
+		, if(cal56_1_sqm.Price is not null
+			, cal56_1_sqm.Price_Date
+			, if(cal_hip.Price is not null
+				, cal_hip.Price_Date
+				, if(cal_bgsq.Price is not null
+					, cal_bgsq.Price_Date
+					, if(cal_d1sq.Price is not null
+						, cal_d1sq.Price_Date
+						, null))))
+		, if(cal_hip.Price is not null
+			, cal_hip.Price_Date
+			, if(cal56_1_sqm.Price is not null
+				, cal56_1_sqm.Price_Date
+				, if(cal_bgsq.Price is not null
+					, cal_bgsq.Price_Date
+					, if(cal_d1sq.Price is not null
+						, cal_d1sq.Price_Date
+						, null))))) as Condo_Price_Per_Square_Date
+	, if(cal_bg_u.Price is not null or cal_d1_u.Price is not null
+		, 'ราคาเริ่มต้น'
+		, if(cal56_1_u.Price is not null
+			, 'ราคาเฉลี่ย'
+			, '')) as Condo_Price_Per_Unit_Text
+	, ifnull(cal_bg_u.Price
+		, ifnull(cal_d1_u.Price, cal56_1_u.Price)) as Condo_Price_Per_Unit
+	, if(cal_bg_u.Price is not null
+		, cal_bg_u.Price_Date
+		, if(cal_d1_u.Price is not null
+			, cal_d1_u.Price_Date
+			, if(cal56_1_u.Price is not null
+				, cal56_1_u.Price_Date
+				, null))) as Condo_Price_Per_Unit_Date
+	, if(cal_561_mb.Data_Value is not null
+		, if((cal_561_mb.Data_Value/100) <= 0
+			, 'PRESALE'
+			, if((cal_561_mb.Data_Value/100) >= 1
+				, 'RESALE'
+				, round((cal_561_mb.Data_Value/100), 2)))
+		, if(b.Condo_Built_Finished is not null
+			, if((b.Condo_Built_Finished + interval 5 year) < now()
+				, 'RESALE'
+				, null)
+			, if(b.Condo_Built_Start is not null
+				, if(a.Condo_HighRise = 1
+					, if((b.Condo_Built_Start + interval 9 year) < now()
+						, 'RESALE'
+						, null)
+					, if((b.Condo_Built_Start + interval 8 year) < now()
+						, 'RESALE'
+						, null))
+				, 'RESALE'))) as Condo_Sold_Status_Show_Value
+	, cal_561_mb.Data_Date as Condo_Sold_Status_Date
+	, if(b.Condo_Built_Finished is not null
+		, if(b.Condo_Built_Finished > now()
+			, 'คาดว่าจะแล้วเสร็จ'
+			, 'ปีที่แล้วเสร็จ')
+		, if(b.Condo_Built_Start is not null
+			, 'ปีที่เปิดตัว'
+			, null)) as Condo_Built_Text
+	, if(b.Condo_Built_Finished is not null
+		, year(b.Condo_Built_Finished)
+		, if(b.Condo_Built_Start is not null
+			, year(b.Condo_Built_Start)
+			, null)) as Condo_Built_Date
+	, if(b.Condo_Built_Start is not null
+		, b.Condo_Built_Start
+		, if(b.Condo_Built_Finished is not null
+			, if(a.Condo_HighRise = 1
+				, b.Condo_Built_Finished - interval 4 year
+				, b.Condo_Built_Finished - interval 3 year)
+			, null)) as Condo_Date_Calculate
+	, if(if(b.Condo_Built_Finished is not null
+			, if((year(curdate()) - (year(b.Condo_Built_Finished) + 1)) > 0
+				, 'OLD'
+				, 'NEW')
+			, if(b.Condo_Built_Start is not null
+				, if(a.Condo_HighRise = 1
+					, if((year(curdate()) - (year(b.Condo_Built_Start) + 4)) > 0
+						, 'OLD'
+						, 'NEW')
+					, if((year(curdate()) - (year(b.Condo_Built_Start) + 3)) > 0
+						, 'OLD'
+						, 'NEW'))
+				, 'OLD')) = 'NEW'
+		, ifnull(cal56_1_sqm.Price
+			, cal_hip.Price)
+		, ifnull(cal_hip.Price
+			, cal56_1_sqm.Price)) as Condo_Price_Per_Square_New
+	, ifnull(cal_bg_u.Price
+		, cal_d1_u.Price) as Condo_Price_Per_Unit_New	
 from real_condo a
+left join real_condo_price b on a.Condo_Code = b.Condo_Code
+left join ( select Condo_Code
+				, Price
+				, Price_Date
+				, Condo_Build_Date
+				, Start_or_AVG
+				, Price_Source
+				, Price_Type
+				, Special
+				, Remark
+			from ( SELECT * 
+					, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Price_Date desc) AS Myorder
+					FROM all_price_view
+					where Price_Source = 1
+					and Price_Type = 'บ/ตรม') order56_1_sqm
+					where Myorder = 1) cal56_1_sqm
+on a.Condo_Code = cal56_1_sqm.Condo_Code
+left join ( select Condo_Code
+				, Price
+				, Price_Date
+				, Condo_Build_Date
+				, Start_or_AVG
+				, Price_Source
+				, Price_Type
+				, Special
+				, Remark
+			from ( SELECT * 
+					, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Price_Date desc) AS Myorder
+					FROM all_price_view
+					where Price_Source = 2) order_hip
+					where Myorder = 1) cal_hip
+on a.Condo_Code = cal_hip.Condo_Code
+left join ( select Condo_Code
+				, Price
+				, Price_Date
+				, Condo_Build_Date
+				, Start_or_AVG
+				, Price_Source
+				, Price_Type
+				, Special
+				, Remark
+			from ( SELECT * 
+					, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Price_Date desc) AS Myorder
+					FROM all_price_view
+					where Price_Source = 4) order_bgsq
+					where Myorder = 1) cal_bgsq
+on a.Condo_Code = cal_bgsq.Condo_Code
+left join ( select Condo_Code
+				, Price
+				, Price_Date
+				, Condo_Build_Date
+				, Start_or_AVG
+				, Price_Source
+				, Price_Type
+				, Special
+				, Remark
+			from ( SELECT * 
+					, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Price_Date desc) AS Myorder
+					FROM all_price_view
+					where Price_Source = 5) order_d1sq
+					where Myorder = 1) cal_d1sq
+on a.Condo_Code = cal_d1sq.Condo_Code
+left join ( select Condo_Code
+				, Price
+				, Price_Date
+				, Condo_Build_Date
+				, Start_or_AVG
+				, Price_Source
+				, Price_Type
+				, Special
+				, Remark
+			from ( SELECT * 
+					, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Price_Date desc) AS Myorder
+					FROM all_price_view
+					where Price_Source = 6) order_bg_u
+					where Myorder = 1) cal_bg_u
+on a.Condo_Code = cal_bg_u.Condo_Code
+left join ( select Condo_Code
+				, Price
+				, Price_Date
+				, Condo_Build_Date
+				, Start_or_AVG
+				, Price_Source
+				, Price_Type
+				, Special
+				, Remark
+			from ( SELECT * 
+					, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Price_Date desc) AS Myorder
+					FROM all_price_view
+					where Price_Source = 7) order_d1_u
+					where Myorder = 1) cal_d1_u
+on a.Condo_Code = cal_d1_u.Condo_Code
+left join ( select Condo_Code
+				, Price
+				, Price_Date
+				, Condo_Build_Date
+				, Start_or_AVG
+				, Price_Source
+				, Price_Type
+				, Special
+				, Remark
+			from ( SELECT * 
+					, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Price_Date desc) AS Myorder
+					FROM all_price_view
+					where Price_Source = 1
+					and Price_Type = 'บ/ยูนิต') order56_1_u
+					where Myorder = 1) cal56_1_u
+on a.Condo_Code = cal56_1_u.Condo_Code
+left join ( select * 
+			from (	select Condo_Code
+						, Data_Date
+						, Data_Attribute
+						, Data_Value
+						, Data_Note
+						, ROW_NUMBER() OVER (PARTITION BY Condo_Code ORDER BY Data_Date desc) AS Myorder
+					from real_condo_561
+					where Data_Status = 1
+					and Data_Attribute = 'sold_percent') order_561_mb
+			where Myorder = 1) cal_561_mb
+on a.Condo_Code = cal_561_mb.Condo_Code			
 WHERE a.Condo_Latitude is not null
 AND a.Condo_Longitude is not null
 AND a.Province_ID in (10, 11, 12, 13, 73, 74)
