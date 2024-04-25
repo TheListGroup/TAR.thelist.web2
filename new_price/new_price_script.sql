111225d5a1beb3452b81b38262640bc74eaa95d8
TAR.thelist.web2 <warintorn.realist@gmail.com>
2024-04-25 17:17:27 +0700

    more note

--- new_price/new_price_script.sql
+++ new_price/new_price_script.sql
@@ -96,7 +96,7 @@ where Price_Start_Day1_Unit is not null;
 
 update `real_condo_561` set Data_Note = '' where Data_Note in ('56-1','56-7');
 
--- 56-1 to House_Type 1
+-- 56-1 to Price_Type 1
 insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
 select Condo_Code
     , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
@@ -117,7 +117,7 @@ and Data_Note in ('BLOGGER','BlOGGER');
 
 delete from real_condo_561 where Data_Note in ('BLOGGER','BlOGGER') and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
 
--- 56-1 to House_Type 10
+-- 56-1 to Price_Type 10
 insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
 select Condo_Code
     , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
@@ -138,7 +138,7 @@ and Data_Note in ('ร่างหนังสือชี้ชวน','Officia
 
 delete from real_condo_561 where Data_Note in ('ร่างหนังสือชี้ชวน','Official') and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
 
--- 56-1 to House_Type 12
+-- 56-1 to Price_Type 12
 insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
 select Condo_Code
     , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
@@ -159,7 +159,7 @@ and Data_Note like '%list%';
 
 delete from real_condo_561 where Data_Note like '%list%' and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
 
--- 56-1 to House_Type 4
+-- 56-1 to Price_Type 4
 insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
 select Condo_Code
     , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
@@ -180,7 +180,7 @@ and Data_Note like '%think%';
 
 delete from real_condo_561 where Data_Note like '%think%' and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
 
--- 56-1 to House_Type 2
+-- 56-1 to Price_Type 2
 insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
 select Condo_Code
     , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
@@ -201,7 +201,7 @@ and Data_Note like '%condonayoo%';
 
 delete from real_condo_561 where Data_Note like '%condonayoo%' and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
 
--- 56-1 to House_Type 5
+-- 56-1 to Price_Type 5
 insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
 select Condo_Code
     , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
@@ -222,7 +222,7 @@ and Data_Note like '%condotiddoi%';
 
 delete from real_condo_561 where Data_Note like '%condotiddoi%' and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
 
--- 56-1 to House_Type 11
+-- 56-1 to Price_Type 11
 insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
 select Condo_Code
     , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
@@ -239,6 +239,25 @@ select Condo_Code
 from real_condo_561
 where Data_Status = 1
 and Data_Attribute in ('price_per_unit_mb','price_per_sqm')
-and Data_Note = 'พี่บีม Official';
+and Data_Note in ('พี่บีม Official','พี่บีมส่งข้อมูลมาให้ google drive','พี่ตอบอก : มีอัพเดตข้อมูลมา');
 
-delete from real_condo_561 where Data_Note = 'พี่บีม Official' and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
+-- 56-1 to Price_Type 12
+insert into real_condo_price_new (Condo_Code, Price, Price_Date, Condo_Build_Date, Start_or_AVG, Price_Source, Price_Type, Special, Remark, Price_Status, Created_By, Last_Updated_By)
+select Condo_Code
+    , if(Data_Attribute='price_per_sqm',round(Data_Value),round(Data_Value*1000000,-4)) as Price
+    , Data_Date as Price_Date
+    , '0' as Condo_Build_Date
+    , 'เฉลี่ย' as Start_or_Average
+    , 12 as Price_Source
+    , if(Data_Attribute='price_per_sqm','บ/ตรม','บ/ยูนิต') as Price_Type
+    , '0' as Special
+    , NULL as Remark
+    , '1' as Price_Status
+    , 32 as Created_By
+    , 32 as Last_Updated_By
+from real_condo_561
+where Data_Status = 1
+and Data_Attribute in ('price_per_unit_mb','price_per_sqm')
+and Data_Note = 'ตตบอก';
+
+delete from real_condo_561 where Data_Note in ('พี่บีม Official','พี่บีมส่งข้อมูลมาให้ google drive','พี่ตอบอก : มีอัพเดตข้อมูลมา','ตตบอก') and Data_Attribute in ('price_per_unit_mb','price_per_sqm');
