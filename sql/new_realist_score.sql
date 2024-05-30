5efbbb161c25d3106610be055cc345b0ee0eaf0a
TAR.thelist.web2 <warintorn.realist@gmail.com>
2024-05-30 18:47:08 +0700

    ืuse Published_Date and Budget to Cal Score

--- sql/new_realist_score.sql
+++ sql/new_realist_score.sql
@@ -1,57 +1,15 @@
 create or replace VIEW `realist_score` AS
-SELECT
-    ads_base.Prop_Code AS Condo_Code,
-    sum(
-        if(
-            (
-                (
-                    (
-                        (
-                            to_days(
-                                (
-                                    date(ads_base.Published_Date) + interval (ads_base.Show_Days + ads_base.Left_Days) day
-                                )
-                            ) - to_days(now())
-                        ) * 0.005479452055
-                    ) + 8
-                ) < 2
-            ),
-            2,
-            if(
-                (
-                    (
-                        (
-                            (
-                                to_days(
-                                    (
-                                        date(ads_base.Published_Date) + interval (ads_base.Show_Days + ads_base.Left_Days) day
-                                    )
-                                ) - to_days(now())
-                            ) * 0.005479452055
-                        ) + 8
-                    ) > 10
-                ),
-                10,
-                (
-                    (
-                        (
-                            to_days(
-                                (
-                                    date(ads_base.Published_Date) + interval (ads_base.Show_Days + ads_base.Left_Days) day
-                                )
-                            ) - to_days(now())
-                        ) * 0.005479452055
-                    ) + 8
-                )
-            )
-        )
-    ) AS `Realist_Score`
-FROM
-    ads_base
-where
-    ads_base.AD_Status = '1'
-    and ads_base.Prop_Type = 'CD'
-group by
-    ads_base.Prop_Code
-order by
-    ads_base.Prop_Code;
+select Condo_Code
+    , sum(Score_cal) as Realist_Score
+from (SELECT Prop_Code AS Condo_Code
+        , if(AD_Type = 'Auto', Auto_AD_Budget, Manual_AD_Day * 7500) as AD_Budget
+        , least(if(AD_Type = 'Auto', Auto_AD_Budget / 7500, Manual_AD_Day), 10) as Start_Score
+        , to_days(date(Published_Date)) - to_days(now()) as interval_day
+        , GREATEST(LEAST(if(AD_Type = 'Auto', Auto_AD_Budget / 7500, Manual_AD_Day), 10) - ((DATEDIFF(CURDATE(), Published_Date) / 365) * 0.2 
+            * LEAST(if(AD_Type = 'Auto', Auto_AD_Budget / 7500, Manual_AD_Day), 10))
+            , 0.2 * LEAST(if(AD_Type = 'Auto', Auto_AD_Budget / 7500, Manual_AD_Day), 10)) as Score_cal
+    FROM ads_base
+    WHERE ((ads_base.AD_Status = '1') AND (ads_base.Prop_Type = 'CD'))
+    ORDER BY ads_base.Prop_Code ASC) cal
+group by Condo_Code
+ORDER BY Condo_Code ASC;
