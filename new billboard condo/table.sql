1c8e21ee00c79734e4b7a1e1394b4a40aed7ee9f
TAR.thelist.web2 <warintorn.realist@gmail.com>
2024-07-26 14:07:30 +0700

    billboard

---
+++ new billboard condo/table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `billboard` (
+    ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Title_Text varchar(100) not null,
+    Subtitle_Text varchar(100) not null,
+    Description_Text text not null,
+    Button_Text varchar(50) not null,
+    Billboard_URL text not null,
+    Billboard_Image text not null,
+    Billboard_Order smallint not null,
+    Page_Type enum('template','developer','brand') not null,
+    Billboard_Code varchar(50) not null,
+    Billboard_Status smallint not null DEFAULT 0,
+    PRIMARY KEY (ID)
+) ENGINE = InnoDB;
