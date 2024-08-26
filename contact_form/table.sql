fbdf7d469ef6f029932cffbf1afd25587cea844c
TAR.thelist.web2 <warintorn.realist@gmail.com>
2024-08-26 19:01:27 +0700

    +procedure

--- contact_form/table.sql
+++ contact_form/table.sql
@@ -3,6 +3,8 @@
     -- real_contact_condo_send_to_who
     -- real_contact_condo_send_to_who_log
 -- real_contact_dev_agent_detail_view
+-- table real_contact_dev_agent_detail
+-- proc real_contact_dev_agent_detail
 
 -- Table real_contact_dev_agent
 CREATE TABLE `real_contact_dev_agent` (
@@ -54,7 +56,7 @@ CREATE TABLE `real_contact_condo_send_to_who_log` (
 ) ENGINE=InnoDB;
 
 -- view real_contact_dev_agent_detail
-CREATE OR REPLACE VIEW real_contact_dev_agent_detail as
+CREATE OR REPLACE VIEW source_real_contact_dev_agent_detail as
 select rc.Condo_Code
     , sm.Condo_Sold_Out
     , if(sm.Condo_Sold_Out = 1
@@ -108,5 +110,95 @@ left join (select Condo_Code
                     or (Condo_Sold_Status_Show_Value = 'RESALE'),1,0) as Condo_Sold_Out
             from all_condo_price_calculate) sm 
 on rc.Condo_Code = sm.Condo_Code
-where rc.Condo_Status <> 2
+where rc.Condo_Status = 1
 order by rc.Condo_Code, da.Dev_or_Agent;
+
+-- table real_contact_dev_agent_detail
+CREATE TABLE `real_contact_dev_agent_detail` (
+    `ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Condo_Code` varchar(50) NOT NULL,
+    `Condo_Sold_Out` SMALLINT UNSIGNED NULL,
+    `Dev_or_Agent` ENUM('D','A') NOT NULL,
+    `Dev_Agent_Contact_ID` INT UNSIGNED NULL,
+    `Company_Name` varchar(250) NULL DEFAULT '',
+    `Contact_Name` varchar(250) NULL DEFAULT '',
+    `Email` text NULL,
+    PRIMARY KEY (`ID`)
+) ENGINE=InnoDB;
+
+-- proc real_contact_dev_agent_detail
+DROP PROCEDURE IF EXISTS truncateInsert_real_contact_dev_agent_detail;
+DELIMITER //
+
+CREATE PROCEDURE truncateInsert_real_contact_dev_agent_detail ()
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    DECLARE total_rows INT DEFAULT 0;
+    DECLARE v_name VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name1 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name2 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name3 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name4 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name5 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name6 text DEFAULT NULL;
+
+    DECLARE proc_name       VARCHAR(50) DEFAULT 'truncateInsert_real_contact_dev_agent_detail';
+    DECLARE code            VARCHAR(10) DEFAULT '00000';
+    DECLARE msg             TEXT;
+    DECLARE rowCount        INTEGER     DEFAULT 0;
+    DECLARE nrows           INTEGER     DEFAULT 0;
+    DECLARE errorcheck      BOOLEAN  DEFAULT 1;
+    DECLARE done INT DEFAULT FALSE;
+
+    DECLARE cur CURSOR FOR SELECT Condo_Code, Condo_Sold_Out, Dev_or_Agent, Dev_Agent_Contact_ID, Company_Name, Contact_Name, Email
+                            FROM source_real_contact_dev_agent_detail;
+
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1
+            code = RETURNED_SQLSTATE, msg = MESSAGE_TEXT;
+            SET msg = CONCAT(msg,' AT ',v_name, ' -- ',v_name3);
+        INSERT INTO realist_log (Type, SQL_State, Message, Location) VALUES(1, code, msg, proc_name);
+        set errorcheck = 0;
+        
+    END;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    TRUNCATE TABLE real_contact_dev_agent_detail;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO v_name,v_name1,v_name2,v_name3,v_name4,v_name5,v_name6;
+
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        INSERT INTO
+            real_contact_dev_agent_detail(
+                `Condo_Code`,
+                `Condo_Sold_Out`,
+                `Dev_or_Agent`,
+                `Dev_Agent_Contact_ID`,
+                `Company_Name`,
+                `Contact_Name`,
+                `Email`
+                )
+        VALUES(v_name,v_name1,v_name2,v_name3,v_name4,v_name5,v_name6);
+        
+        GET DIAGNOSTICS nrows = ROW_COUNT;
+        SET total_rows = total_rows + nrows;
+        SET i = i + 1;
+    END LOOP;
+
+    if errorcheck then
+        SET code    = '00000';
+        SET msg     = CONCAT(total_rows,' rows inserted.');
+        INSERT INTO realist_log (Type, SQL_State, Message, Location) VALUES(0,code , msg, proc_name);
+    end if;
+
+    CLOSE cur;
+END //
+DELIMITER ;
