e2f5ef6cf05c57473197c1832d0022976fdfbdd2
TAR.thelist.web2 <warintorn.realist@gmail.com>
2024-10-17 15:22:09 +0700

    condo_article

--- Samie/query.sql
+++ Samie/query.sql
@@ -579,3 +579,30 @@ select 'shophouse'
     , concat("https://thelist.group/realist/housing/shophouse/list/spotlight/",hs.Spotlight_Name) as 'Link'
 from housing_spotlight hs
 where hs.Spotlight_Inactive = 0;
+
+
+SELECT 
+  wp.ID, 
+  wp.post_date, 
+  wp.post_title,
+  ifnull(pm.aaa_condo,'') as Condo_Code,
+  tag_type.tag_name,
+  CONCAT('https://thelist.group/realist/blog/', wp.post_name) AS Link
+FROM wp_posts wp
+left join (select post_id, group_concat(meta_value separator ', ') AS aaa_condo
+          from wp_postmeta 
+          where meta_key = 'aaa_condo'
+          and meta_value <> ''
+          group by post_id) pm
+on wp.ID = pm.post_id
+LEFT JOIN (select wpr.object_id, group_concat(wt.name separator ', ') as tag_name 
+          from wp_term_relationships wpr
+          left join wp_term_taxonomy wtt on wpr.term_taxonomy_id = wtt.term_taxonomy_id
+          left join wp_terms wt on wtt.term_id = wt.term_id
+          -- where wtt.term_id in (6,7,156,760,867,869,972)
+          group by wpr.object_id) tag_type
+ON wp.ID = tag_type.object_id
+WHERE wp.post_status = 'publish'
+AND wp.post_password = ''
+and tag_type.tag_name is not null
+ORDER BY wp.ID DESC;
