4b8c3913b3f4e5947534dd328a415eef3f293dc8
TAR.thelist.web2 <warintorn.realist@gmail.com>
2024-11-19 17:44:34 +0700

    sc asset

--- P Win/conatct.sql
+++ P Win/conatct.sql
@@ -7,3 +7,14 @@ and (rcf.Contact_Date like '2024-05%' or rcf.Contact_Date like '2024-06%')
 and cd.Developer_ENName is not null
 group by cd.Developer_ENName
 ORDER BY cd.Developer_ENName;
+
+
+
+-- sc asset
+SELECT rcf.*
+    , rc.Condo_ENName
+FROM `real_contact_form` rcf
+left join real_condo rc on rcf.Contact_Ref_ID = rc.Condo_Code
+left join condo_developer cd on rc.Developer_Code = cd.Developer_Code
+where rcf.Contact_Type = 'contact'
+and cd.Developer_Code = 'DV0319-00';
