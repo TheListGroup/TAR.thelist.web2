82ec7cf84943ac95d9c0edcf3f38cbbbca49033c
TAR.thelist.web2 <warintorn.realist@gmail.com>
2024-11-19 17:44:12 +0700

    detail view

--- classifield/classified_view.sql
+++ classifield/classified_view.sql
@@ -8,7 +8,7 @@
 ALTER TABLE `classified_list_view` ADD `User_ID` INT UNSIGNED NULL AFTER `Price_Rent_Sort`;
 create or replace view source_classified_list_view as
 select c.Classified_ID
-    , concat_ws(' ',ifnull(if(c.Room_Type='Studio','1 Bed',REPLACE(c.Room_Type,'room','')),concat(c.BedRoom,' Bed')),concat(c.BathRoom,' Bath'),c.Unit_Floor_Type) as Unit_Type
+    , concat_ws(' ',ifnull(if(c.Room_Type='Studio','1 Bed',REPLACE(REPLACE(c.Room_Type,'rooms',''),'room','')),concat(c.BedRoom,' Bed')),concat(c.BathRoom,' Bath'),c.Unit_Floor_Type) as Unit_Type
     , ifnull(ci.Classified_Image_URL,ifnull(fi.Image_URL,ifnull(concat('/realist/condo/uploads/condo/',c.Condo_Code,'/',c.Condo_Code,'-PE-01-Exterior-H-240.webp'),null))) as Classified_Image
     , nun(REPLACE(format(Size, 2),',','')) as Size
     , nun(c.Bedroom) as Bedroom
@@ -35,8 +35,9 @@ left join (SELECT Classified_ID
                         ,Classified_Image_URL
                         , ROW_NUMBER() OVER (PARTITION BY Classified_ID ORDER BY Displayed_Order_in_Classified) AS RowNum
                 FROM classified_image
-                where Classified_Image_Status = '1') sub
-            WHERE RowNum = 1) ci 
+                where Classified_Image_Status = '1'
+                and Classified_Image_Type = 1) sub
+            WHERE RowNum = 1) ci
 on c.Classified_ID = ci.Classified_ID
 left join (SELECT Condo_Code
                 , concat('/realist/condo/uploads/condo/',Condo_Code,'/',Image_URL) as Image_URL
@@ -169,7 +170,7 @@ DELIMITER ;
 
 create or replace view source_classified_detail_view as
 select c.Classified_ID 
-    , concat_ws(' ',ifnull(if(c.Room_Type='Studio','1 Bed',REPLACE(c.Room_Type,'room','')),concat(c.BedRoom,' Bed')),concat(c.BathRoom,' Bath'),c.Unit_Floor_Type,concat(format(c.Size,1),' sq.m.')) as Unit_Type
+    , concat_ws(' ',ifnull(if(c.Room_Type='Studio','1 Bed',REPLACE(REPLACE(c.Room_Type,'rooms',''),'room','')),concat(c.BedRoom,' Bed')),concat(c.BathRoom,' Bath'),c.Unit_Floor_Type,concat(format(c.Size,1),' sq.m.')) as Unit_Type
     , namee.condo_name as Condo_Name
     , c.Condo_Code as Condo_Code
     , concat(if(length(day(c.Last_Updated_Date))=2,day(c.Last_Updated_Date),concat("0",day(c.Last_Updated_Date))),'/'
@@ -187,7 +188,7 @@ select c.Classified_ID
     , ifnull(c.Furnish,null) as Furnish
     , if(c.Furnish is not null,1,null) as Furnish_Status
     , ifnull(classified_fac.fac_room,null) as Facilities
-    , ifnull(concat_ws('\n\n',c.Title_TH,c.Title_ENG,c.Descriptions_TH,c.Descriptions_Eng),null) as Description
+    , c.Title_TH as Title_TH
     , ifnull(concat('ค่าโอนกรรมสิทธิ์ : ',c.Sale_Transfer_Fee,' %'),null) as Sale_Transfer_Fee
     , ifnull(concat('(',format(round(((c.Sale_Transfer_Fee/100)*c.Price_Sale)),0),' บ.)'),null) as Sub_Sale_Transfer_Fee
     , ifnull(concat('เงินมัดจำ(ซื้อ) : ',c.Sale_Deposit,' %'),null) as Sale_Deposit
@@ -197,11 +198,11 @@ select c.Classified_ID
     , ifnull(concat('ค่าส่วนกลาง : ',rcp.Condo_Common_Fee,' บ./ตร.ม./เดือน'),null) as Condo_Common_Fee
     , ifnull(concat('(',format((rcp.Condo_Common_Fee*c.Size),2),' บ.)'),null) as Sub_Condo_Common_Fee
     , ifnull(concat('สัญญาเช่าขั้นต่ำ : ',c.Min_Rental_Contract,' เดือน'),null) as Min_Rental_Contract
-    , ifnull(concat('(',format(round((c.Min_Rental_Contract*c.Price_Rent)),0),' บ.)'),null) as Sub_Min_Rental_Contract
+    , ifnull(concat('(',format(round((CAST(CAST(c.Min_Rental_Contract AS CHAR) AS UNSIGNED)*c.Price_Rent)),0),' บ.)'),null) as Sub_Min_Rental_Contract
     , ifnull(concat('เงินมัดจำ/ประกัน(เช่า) : ',c.Rent_Deposit,' เดือน'),null) as Rent_Deposit
-    , ifnull(concat('(',format(round((c.Rent_Deposit*c.Price_Rent)),0),' บ.)'),null) as Sub_Rent_Deposit
+    , ifnull(concat('(',format(round((CAST(CAST(c.Rent_Deposit AS CHAR) AS UNSIGNED)*c.Price_Rent)),0),' บ.)'),null) as Sub_Rent_Deposit 
     , ifnull(concat('จ่ายล่วงหน้า : ',c.Advance_Payment,' เดือน'),null) as Advance_Payment
-    , ifnull(concat('(',format(round((c.Advance_Payment*c.Price_Rent)),0),' บ.)'),null) as Sub_Advance_Payment
+    , ifnull(concat('(',format(round((CAST(CAST(c.Advance_Payment AS CHAR) AS UNSIGNED)*c.Price_Rent)),0),' บ.)'),null) as Sub_Advance_Payment
     , cu.Email as Mail
     , c.Classified_Status as Classified_Status
     , cu.First_Name as Agent_Name
@@ -237,6 +238,16 @@ select c.Classified_ID
                                 , concat('คอนโดใกล้รฟฟ.สถานี', sub_station.Station)
                                 , concat('คอนโด ', namee.condo_name))))
                     , '| REAL DATA รวมข้อมูลคอนโด ประกาศขายเช่า') as Classified_Description
+    , c.User_ID as User_ID
+    , c.Floor as Floor
+    , c.Direction as Direction
+    , c.Move_In as Move_In
+    , c.Parking as Parking
+    , c.Unit_Floor_Type as Unit_Floor_Type
+    , c.Sale_with_Tenant as Sale_with_Tenant
+    , c.Title_ENG as Title_ENG
+    , c.Descriptions_TH as Descriptions_TH
+    , c.Descriptions_Eng as Descriptions_Eng
 from classified c
 left join ( SELECT rc.Condo_Code, 
                 if(Condo_ENName1 is not null
@@ -258,6 +269,7 @@ left join ( SELECT rc.Condo_Code,
             ORDER BY rc.Condo_Code ) namee
 on c.Condo_Code = namee.Condo_Code
 left join (select Classified_ID,JSON_ARRAYAGG( JSON_OBJECT(  'Classified_Image_ID',Classified_Image_ID
+                                                            , 'Classified_Image_Type',Classified_Image_Type
                                                             , 'Classified_Image_Caption',Classified_Image_Caption
                                                             , 'Classified_Image_URL',concat('/realist/condo/uploads/classified/',lpad(Classified_ID,6,0),'/',Classified_Image_URL)
                                                             , 'Displayed_Order_in_Classified',Displayed_Order_in_Classified)) as Image
@@ -309,6 +321,17 @@ ALTER TABLE classified_detail_view ADD Sale BOOLEAN NULL AFTER Condo_Longitude;
 ALTER TABLE classified_detail_view ADD Rent BOOLEAN NULL AFTER Sale;
 ALTER TABLE classified_detail_view ADD Classified_Title TEXT NULL AFTER Rent;
 ALTER TABLE classified_detail_view ADD Classified_Description TEXT NULL AFTER Classified_Title;
+ALTER TABLE classified_detail_view ADD User_ID INT UNSIGNED NULL AFTER Classified_Description;
+ALTER TABLE classified_detail_view ADD Floor SMALLINT UNSIGNED NULL AFTER User_ID;
+ALTER TABLE classified_detail_view ADD Direction ENUM('ทิศเหนือ','ทิศใต้','ทิศตะวันออก','ทิศตะวันตก','ทิศตะวันออกเฉียงเหนือ','ทิศตะวันออกเฉียงใต้','ทิศตะวันตกเฉียงเหนือ','ทิศตะวันตกเฉียงใต้') NULL AFTER Floor;
+ALTER TABLE classified_detail_view ADD Move_In enum('พร้อมให้เข้าอยู่', 'ภายใน 1 - 3 เดือน') NULL AFTER Direction;
+ALTER TABLE classified_detail_view ADD Parking BOOLEAN NULL AFTER Move_In;
+ALTER TABLE classified_detail_view ADD Unit_Floor_Type enum('Loft', 'Simplex', 'Duplex', 'Triplex') NULL AFTER Parking;
+ALTER TABLE classified_detail_view ADD Sale_with_Tenant BOOLEAN NULL AFTER Unit_Floor_Type;
+ALTER TABLE classified_detail_view CHANGE Description Title_TH TEXT NULL;
+ALTER TABLE classified_detail_view ADD Title_ENG TEXT NULL AFTER Sale_with_Tenant;
+ALTER TABLE classified_detail_view ADD Descriptions_Eng TEXT NULL AFTER Title_ENG;
+ALTER TABLE classified_detail_view ADD Descriptions_TH TEXT NULL AFTER Descriptions_Eng;
 
 -- Table `classified_detail_view`
 CREATE TABLE IF NOT EXISTS `classified_detail_view` (
@@ -327,10 +350,10 @@ CREATE TABLE IF NOT EXISTS `classified_detail_view` (
     `Unit_Size` FLOAT(7,2) NULL,
     `Bedroom` SMALLINT UNSIGNED NOT NULL,
     `Bathroom` SMALLINT UNSIGNED NULL,
-    `Furnish` ENUM('Fully Fitted','Fully Furnished') NULL,
+    `Furnish` ENUM('Bareshell','Non Furnished','Fully Fitted','Fully Furnished') NULL,
     `Furnish_Status` SMALLINT UNSIGNED NULL,
     `Facilities` JSON NULL,
-    `Description` TEXT NULL,
+    `Title_TH` TEXT NULL,
     `Sale_Transfer_Fee` VARCHAR(150) NULL,
     `Sub_Sale_Transfer_Fee` VARCHAR(150) NULL,
     `Sale_Deposit` VARCHAR(150) NULL,
@@ -357,6 +380,16 @@ CREATE TABLE IF NOT EXISTS `classified_detail_view` (
     `Rent` BOOLEAN NULL,
     `Classified_Title` TEXT NULL,
     `Classified_Description` TEXT NULL,
+    `User_ID` INT UNSIGNED NULL,
+    `Floor` SMALLINT UNSIGNED NULL,
+    `Direction` ENUM('ทิศเหนือ','ทิศใต้','ทิศตะวันออก','ทิศตะวันตก','ทิศตะวันออกเฉียงเหนือ','ทิศตะวันออกเฉียงใต้','ทิศตะวันตกเฉียงเหนือ','ทิศตะวันตกเฉียงใต้') NULL,
+    `Move_In` enum('พร้อมให้เข้าอยู่', 'ภายใน 1 - 3 เดือน') NULL,
+    `Parking` BOOLEAN NULL,
+    `Unit_Floor_Type` enum('Loft', 'Simplex', 'Duplex', 'Triplex') NULL,
+    `Sale_with_Tenant` BOOLEAN NULL,
+    `Title_ENG` TEXT NULL,
+    `Descriptions_Eng` TEXT NULL,
+    `Descriptions_TH` TEXT NULL,
     PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
@@ -412,6 +445,16 @@ BEGIN
     DECLARE v_name41 VARCHAR(250) DEFAULT NULL;
     DECLARE v_name42 TEXT DEFAULT NULL;
     DECLARE v_name43 TEXT DEFAULT NULL;
+    DECLARE v_name44 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name45 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name46 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name47 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name48 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name49 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name50 VARCHAR(250) DEFAULT NULL;
+    DECLARE v_name51 TEXT DEFAULT NULL;
+    DECLARE v_name52 TEXT DEFAULT NULL;
+    DECLARE v_name53 TEXT DEFAULT NULL;
 
     DECLARE proc_name       VARCHAR(70) DEFAULT 'truncateInsert_classified_detail_view';
     DECLARE code            VARCHAR(10) DEFAULT '00000';
@@ -424,11 +467,12 @@ BEGIN
 
     DECLARE cur CURSOR FOR  SELECT Classified_ID,Unit_Type,Condo_Name,Condo_Code,Last_Updated_Date,Cover_Image,Image,Price_Sale
                                     ,Price_Sale_Per_Square,Price_Rent,Rental_Contract,Unit_Size,Bedroom,Bathroom,Furnish,Furnish_Status
-                                    ,Facilities,Description,Sale_Transfer_Fee,Sub_Sale_Transfer_Fee,Sale_Deposit,Sub_Sale_Deposit
+                                    ,Facilities,Title_TH,Sale_Transfer_Fee,Sub_Sale_Transfer_Fee,Sale_Deposit,Sub_Sale_Deposit
                                     ,Sale_Mortgage_Costs,Sub_Sale_Mortgage_Costs,Condo_Common_Fee,Sub_Condo_Common_Fee,Min_Rental_Contract
                                     ,Sub_Min_Rental_Contract,Rent_Deposit,Sub_Rent_Deposit,Advance_Payment,Sub_Advance_Payment,Mail
                                     ,Classified_Status,Agent_Name,District_Name,SubDistrict_Name,Province_Name,Condo_Latitude
-                                    ,Condo_Longitude,Sale,Rent,Classified_Title,Classified_Description
+                                    ,Condo_Longitude,Sale,Rent,Classified_Title,Classified_Description,User_ID,Floor,Direction,Move_In
+                                    ,Parking,Unit_Floor_Type,Sale_with_Tenant,Title_ENG,Descriptions_Eng,Descriptions_TH
                             FROM source_classified_detail_view;
 
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
@@ -447,7 +491,7 @@ BEGIN
     OPEN cur;
 
     read_loop: LOOP
-        FETCH cur INTO v_name,v_name1,v_name2,v_name3,v_name4,v_name5,v_name6,v_name7,v_name8,v_name9,v_name10,v_name11,v_name12,v_name13,v_name14,v_name15,v_name16,v_name17,v_name18,v_name19,v_name20,v_name21,v_name22,v_name23,v_name24,v_name25,v_name26,v_name27,v_name28,v_name29,v_name30,v_name31,v_name32,v_name33,v_name34,v_name35,v_name36,v_name37,v_name38,v_name39,v_name40,v_name41,v_name42,v_name43;
+        FETCH cur INTO v_name,v_name1,v_name2,v_name3,v_name4,v_name5,v_name6,v_name7,v_name8,v_name9,v_name10,v_name11,v_name12,v_name13,v_name14,v_name15,v_name16,v_name17,v_name18,v_name19,v_name20,v_name21,v_name22,v_name23,v_name24,v_name25,v_name26,v_name27,v_name28,v_name29,v_name30,v_name31,v_name32,v_name33,v_name34,v_name35,v_name36,v_name37,v_name38,v_name39,v_name40,v_name41,v_name42,v_name43,v_name44,v_name45,v_name46,v_name47,v_name48,v_name49,v_name50,v_name51,v_name52,v_name53;
 
         IF done THEN
             LEAVE read_loop;
@@ -472,7 +516,7 @@ BEGIN
                 `Furnish`,
                 `Furnish_Status`,
                 `Facilities`,
-                `Description`,
+                `Title_TH`,
                 `Sale_Transfer_Fee`,
                 `Sub_Sale_Transfer_Fee`,
                 `Sale_Deposit`,
@@ -498,9 +542,19 @@ BEGIN
                 `Sale`,
                 `Rent`,
                 `Classified_Title`,
-                `Classified_Description`
+                `Classified_Description`,
+                `User_ID`,
+                `Floor`,
+                `Direction`,
+                `Move_In`,
+                `Parking`,
+                `Unit_Floor_Type`,
+                `Sale_with_Tenant`,
+                `Title_ENG`,
+                `Descriptions_Eng`,
+                `Descriptions_TH`
                 )
-        VALUES(v_name,v_name1,v_name2,v_name3,v_name4,v_name5,v_name6,v_name7,v_name8,v_name9,v_name10,v_name11,v_name12,v_name13,v_name14,v_name15,v_name16,v_name17,v_name18,v_name19,v_name20,v_name21,v_name22,v_name23,v_name24,v_name25,v_name26,v_name27,v_name28,v_name29,v_name30,v_name31,v_name32,v_name33,v_name34,v_name35,v_name36,v_name37,v_name38,v_name39,v_name40,v_name41,v_name42,v_name43);
+        VALUES(v_name,v_name1,v_name2,v_name3,v_name4,v_name5,v_name6,v_name7,v_name8,v_name9,v_name10,v_name11,v_name12,v_name13,v_name14,v_name15,v_name16,v_name17,v_name18,v_name19,v_name20,v_name21,v_name22,v_name23,v_name24,v_name25,v_name26,v_name27,v_name28,v_name29,v_name30,v_name31,v_name32,v_name33,v_name34,v_name35,v_name36,v_name37,v_name38,v_name39,v_name40,v_name41,v_name42,v_name43,v_name44,v_name45,v_name46,v_name47,v_name48,v_name49,v_name50,v_name51,v_name52,v_name53);
         GET DIAGNOSTICS nrows = ROW_COUNT;
         SET total_rows = total_rows + nrows;
         SET i = i + 1;
@@ -513,43 +567,5 @@ BEGIN
     end if;
 
     CLOSE cur;
-END //
-DELIMITER ;
-
-
-DROP PROCEDURE IF EXISTS Insertclassified_all_logs;
-DELIMITER //
-
-CREATE PROCEDURE Insertclassified_all_logs ()
-BEGIN
-    DECLARE proc_name       VARCHAR(70) DEFAULT 'Insertclassified_all_logs';
-    DECLARE code            VARCHAR(10) DEFAULT '00000';
-    DECLARE msg             TEXT;
-    DECLARE errorcheck      BOOLEAN     DEFAULT 1;
-
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-        GET DIAGNOSTICS CONDITION 1
-            code = RETURNED_SQLSTATE, msg = MESSAGE_TEXT;
-        INSERT INTO realist_log (Type, SQL_State, Message, Location) VALUES(1, code, msg, proc_name);
-        set errorcheck = 0;
-    END;
-
-    INSERT INTO classified_all_logs (Classified_ID, Ref_ID, Project_ID, Title_TH, Title_ENG, Condo_Code, Sale, Sale_with_Tenant
-        , Rent, Price_Sale, Sale_Transfer_Fee, Sale_Deposit, Sale_Mortgage_Costs, Price_Rent, Min_Rental_Contract, Rent_Deposit
-        , Advance_Payment, Room_Type, Unit_Floor_Type, PentHouse, Bedroom, Bathroom, Size, Furnish, Parking, Descriptions_Eng, Descriptions_TH
-        , User_ID, Classified_Status, Created_By, Created_Date, Last_Updated_By, Last_Updated_Date)
-    SELECT Classified_ID, Ref_ID, Project_ID, Title_TH, Title_ENG, Condo_Code, Sale, Sale_with_Tenant, Rent, Price_Sale
-        , Sale_Transfer_Fee, Sale_Deposit, Sale_Mortgage_Costs, Price_Rent, Min_Rental_Contract, Rent_Deposit, Advance_Payment
-        , Room_Type, Unit_Floor_Type, PentHouse, Bedroom, Bathroom, Size, Furnish, Parking, Descriptions_Eng, Descriptions_TH, User_ID, Classified_Status
-        , Created_By, Created_Date, Last_Updated_By, Last_Updated_Date
-    FROM classified;
-
-    if errorcheck then
-        SET code    = '00000';
-        SET msg     = 'Complete';
-        INSERT INTO realist_log (Type, SQL_State, Message, Location) VALUES(0,code , msg, proc_name);
-    end if;
-
 END //
 DELIMITER ;
