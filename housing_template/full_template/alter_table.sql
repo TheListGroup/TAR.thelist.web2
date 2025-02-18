029cdae3cc1b6e871824ee7c201d5ad53ac6210b
TAR.thelist.web2 <warintorn.realist@gmail.com>
2025-02-18 14:24:51 +0700

    show faci column

--- housing_template/full_template/alter_table.sql
+++ housing_template/full_template/alter_table.sql
@@ -6,3 +6,11 @@ ALTER TABLE `housing` ADD `Pool` SMALLINT UNSIGNED NOT NULL DEFAULT '0' AFTER `S
 ALTER TABLE `housing` ADD Pool_System ENUM('เกลือ','Hydrotherapy','คลอรีน','UV','น้ำแร่') NULL AFTER `Pool`;
 ALTER TABLE `housing` ADD Pool_Width FLOAT NULL AFTER `Pool_System`;
 ALTER TABLE `housing` ADD Pool_Length FLOAT NULL AFTER `Pool_Width`;
+
+DROP INDEX h_cate_show_faci ON housing_full_template_category;
+ALTER TABLE `housing_full_template_category` ADD `Show_Faci_order` INT UNSIGNED NULL AFTER `Category_Order`;
+update `housing_full_template_category` set Show_Faci_order = 1 where Category_Show_Faci = 11;
+update `housing_full_template_category` set Show_Faci_order = 2 where Category_Show_Faci = 50;
+update `housing_full_template_category` set Show_Faci_order = 3 where Category_Show_Faci = 2;
+update `housing_full_template_category` set Show_Faci_order = 4 where Category_Show_Faci = 45;
+ALTER TABLE `housing_full_template_element_image_view` ADD `Show_Faci_order` INT UNSIGNED NULL AFTER `Category_Show_Faci`;
