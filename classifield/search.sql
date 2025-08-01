68ac57b0a8e68a70fd81f643982b2d2d61515363
tarr21 <warintorn.realist@gmail.com>
2025-08-01 17:21:07 +0700

    search education

--- classifield/search.sql
+++ classifield/search.sql
@@ -1,3 +1,152 @@
+-- table classified_condo_education
+-- view source_search_classified_education
+-- procedure truncateInsert_classified_condo_education
+-- view source_search_classified
+
+ALTER TABLE `real_place_education` ADD `polygon` polygon NULL AFTER `Education_Boundary`;
+ALTER TABLE `real_place_education` ADD `Condo_Count` INT NOT NULL AFTER `polygon`;
+ALTER TABLE `real_place_education` ADD `Housing_Count` INT NOT NULL AFTER `Condo_Count`;
+ALTER TABLE `real_place_education` ADD `Classified_Unit_Count` INT NOT NULL AFTER `Housing_Count`;
+
+-- table
+CREATE TABLE `classified_condo_education` (
+    `ID` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Classified_ID` int UNSIGNED NOT NULL,
+    `Education` JSON NOT NULL,
+    PRIMARY KEY (`ID`))
+ENGINE=InnoDB;
+
+-- view
+create or replace view source_search_classified_education as
+select c.Classified_ID
+    , CONCAT('[',CONCAT_WS(',',school_only.Place_ID, collage_only.Place_ID),']') AS `Education`
+from classified c
+left join (select Classified_ID
+                , group_concat(Place_ID separator ',') AS Place_ID
+            from (select Classified_ID
+                        , concat('"',Place_ID,'"') as Place_ID
+                        , Place_Name
+                        , Place_category
+                        , Distance
+                    from (SELECT pe.Place_ID
+                                , pe.Place_Name
+                                , pe.Place_category
+                                , pe.Place_Latitude
+                                , pe.Place_Longitude
+                                , c.Classified_ID
+                                , c.Condo_Code
+                                , c.Condo_Latitude
+                                , c.Condo_Longitude
+                                , (6371 * 2 * ASIN(SQRT(POWER(SIN((RADIANS(c.Condo_Latitude - pe.Place_Latitude)) / 2), 2)
+                                    + COS(RADIANS(pe.Place_Latitude)) * COS(RADIANS(c.Condo_Latitude)) *
+                                    POWER(SIN((RADIANS(c.Condo_Longitude - pe.Place_Longitude)) / 2), 2 )))) AS Distance
+                            FROM real_place_education pe
+                            cross join (select a.classified_ID
+                                            , a.Condo_Code
+                                            , b.Condo_Latitude
+                                            , b.Condo_Longitude
+                                        from classified a
+                                        left join real_condo b on a.Condo_Code = b.Condo_Code
+                                        where a.classified_status = '1'
+                                        and b.Condo_Latitude is not null
+                                        and b.Condo_Longitude is not null) c
+                            where pe.Place_category in ('โรงเรียน','โรงเรียนนานาชาติ')) aaa
+                    where aaa.Distance <= 1.8) school
+            group by Classified_ID) school_only
+on c.Classified_ID = school_only.Classified_ID
+left join (select Classified_ID
+                        , group_concat(Place_ID separator ',') AS Place_ID
+                    from (select Classified_ID
+                                , concat('"',Place_ID,'"') as Place_ID
+                                , Place_Name
+                                , Place_category
+                                , 0 as Distance
+                            from (select a.Classified_ID
+                                    , a.Condo_Code
+                                    , b.Condo_Latitude
+                                    , b.Condo_Longitude
+                                    , pe.Place_ID
+                                    , pe.Place_Name
+                                    , pe.Place_category
+                                    , pe.Place_Latitude
+                                    , pe.Place_Longitude
+                                from classified a
+                                join real_condo b on a.Condo_Code = b.Condo_Code
+                                JOIN real_place_education AS pe ON ST_Contains(pe.polygon, ST_GeomFromText(CONCAT('POINT(', b.Condo_Longitude, ' ', b.Condo_Latitude, ')')))
+                                where a.classified_status = '1'
+                                and b.Condo_Latitude is not null
+                                and b.Condo_Longitude is not null
+                                and pe.Place_category not in ('โรงเรียน','โรงเรียนนานาชาติ')) bbb) college
+                group by Classified_ID) collage_only
+on c.Classified_ID = collage_only.Classified_ID
+where c.Classified_Status = '1'
+and (school_only.Place_ID is not null or collage_only.Place_ID is not null)
+group by c.Classified_ID;
+
+-- procedure
+DROP PROCEDURE IF EXISTS truncateInsert_classified_condo_education;
+DELIMITER //
+
+CREATE PROCEDURE truncateInsert_classified_condo_education ()
+BEGIN
+    DECLARE i INT DEFAULT 0;
+	DECLARE total_rows INT DEFAULT 0;
+    DECLARE v_name VARCHAR(250) DEFAULT NULL;
+	DECLARE v_name1 JSON DEFAULT NULL;
+
+	DECLARE proc_name       VARCHAR(50) DEFAULT 'truncateInsert_classified_condo_education';
+    DECLARE code            VARCHAR(10) DEFAULT '00000';
+    DECLARE msg             TEXT;
+    DECLARE rowCount        INTEGER     DEFAULT 0;
+    DECLARE nrows           INTEGER     DEFAULT 0;
+	DECLARE errorcheck      BOOLEAN		DEFAULT 1;
+    DECLARE done INT DEFAULT FALSE;
+
+    DECLARE cur CURSOR FOR SELECT Classified_ID, Education FROM source_search_classified_education;
+	
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1
+            code = RETURNED_SQLSTATE, msg = MESSAGE_TEXT;
+			SET msg = CONCAT(msg,' AT ',v_name);
+        INSERT INTO realist_log (Type, SQL_State, Message, Location) VALUES(1, code, msg, proc_name);
+		set errorcheck = 0;
+    END;
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+	TRUNCATE TABLE classified_condo_education;
+	
+    OPEN cur;
+    read_loop: LOOP
+        FETCH cur INTO v_name,v_name1;
+        
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+		INSERT INTO
+			classified_condo_education (
+				Classified_ID,
+				Education
+			)
+		VALUES(v_name,v_name1);
+		GET DIAGNOSTICS nrows = ROW_COUNT;
+		SET total_rows = total_rows + nrows;
+        SET i = i + 1;
+    END LOOP;
+
+	if errorcheck then
+		SET code    = '00000';
+		SET msg     = CONCAT(total_rows,' rows inserted.');
+		INSERT INTO realist_log (Type, SQL_State, Message, Location) VALUES(0,code , msg, proc_name);
+	end if;
+
+    CLOSE cur;
+END //
+DELIMITER ;
+
+
 -- view search_classified
 create or replace view source_search_classified as
 select c.Classified_ID
@@ -13,7 +162,7 @@ select c.Classified_ID
     , JSON_ARRAY(CAST(cf.Province_ID as CHAR),CAST(cf.District_ID as CHAR),CAST(cf.SubDistrict_ID as CHAR)) as Search_Province
     , JSON_ARRAY(cf.RealDistrict_Code,cf.RealSubDistrict_Code) as Search_Realist_Yarn
     , CONCAT('[',CONCAT_WS(',',condo_line.Condo_Around_Line,station.Condo_Around_Station),']') as Search_Mass_Transit
-    , NULL as Search_University
+    , cce.Education as Search_Education
     , NULL as Search_Airport
     , NULL as Search_Custom_Yarn
     , CONCAT('[',TRIM(TRAILING ',' FROM replace(replace(cf.Spotlight_List,'[','"'),']','",')),']') as Search_Spotlight
@@ -60,6 +209,7 @@ left join (select Condo_Code AS Condo_Code
             group by Condo_Code) station 
 on c.Condo_Code = station.Condo_Code
 left join classified_list_view cv on c.Classified_ID = cv.Classified_ID
+left join classified_condo_education cce on c.Classified_ID = cce.Classified_ID
 where c.Classified_Status = '1';
 
 -- table search_classified
@@ -78,7 +228,7 @@ create table if not exists search_classified (
     Search_Province JSON null,
     Search_Realist_Yarn JSON null,
     Search_Mass_Transit JSON null,
-    Search_University JSON null,
+    Search_Education JSON null,
     Search_Airport JSON null,
     Search_Custom_Yarn JSON null,
     Search_Spotlight JSON null,
@@ -144,7 +294,7 @@ BEGIN
     DECLARE done INT DEFAULT FALSE;
 
     DECLARE cur CURSOR FOR SELECT Classified_ID, Condo_Code, Condo_Age, Title_TH, Title_ENG, Price_Rent, Price_Sale, Bedroom, Bathroom, Size
-                                , Search_Province, Search_Realist_Yarn, Search_Mass_Transit, Search_University, Search_Airport, Search_Custom_Yarn
+                                , Search_Province, Search_Realist_Yarn, Search_Mass_Transit, Search_Education, Search_Airport, Search_Custom_Yarn
                                 , Search_Spotlight, Last_Updated_Date, Condo_Name, Image, Province_Name, District_Name
                                 , Badge_Home, Badge_Listing_or_Template, Condo_Latitude, Condo_Longitude, Condo_ScopeArea, Search_Detail
                             FROM source_search_classified ;
@@ -186,7 +336,7 @@ BEGIN
                 `Search_Province`,
                 `Search_Realist_Yarn`,
                 `Search_Mass_Transit`,
-                `Search_University`,
+                `Search_Education`,
                 `Search_Airport`,
                 `Search_Custom_Yarn`,
                 `Search_Spotlight`,
