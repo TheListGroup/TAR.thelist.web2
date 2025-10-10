561be17aaf4ad5fa1df03a85a278eefbb9673b3d
tarr21 <warintorn.realist@gmail.com>
2025-10-10 11:15:07 +0700

    office home page

--- api/main.py
+++ api/main.py
@@ -6,6 +6,7 @@ from routers.office_unit import router as office_unit_router
 from routers.office_admin_and_leasing_user import router as office_admin_and_leasing_user_router
 from routers.office_project import router as office_project_router
 from routers.office_building import router as office_building_router
+from routers.office_web_home import router as office_web_home_router
 import os
 
 ROOT_PATH = os.getenv("ROOT_PATH", "")  # เว้นว่างได้ถ้าเข้าตรงพอร์ต
@@ -27,3 +28,4 @@ app.include_router(office_unit_router, prefix="/office-units", tags=["office_uni
 app.include_router(office_admin_and_leasing_user_router, prefix="/office-admin-and-leasing-user", tags=["office_admin_and_leasing_user"])
 app.include_router(office_project_router, prefix="/office-project", tags=["office_project"])
 app.include_router(office_building_router, prefix="/office-building", tags=["office_building"])
+app.include_router(office_web_home_router, prefix="/office-web-home", tags=["office_web_home"])
