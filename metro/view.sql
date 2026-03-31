a8f8c13abf4a282142b25e097236cf0c93d37a15
tarr21 <warintorn.realist@gmail.com>
2026-03-31 15:09:29 +0700

    prof_url

--- metro/view.sql
+++ metro/view.sql
@@ -1,5 +1,5 @@
 create or replace view prof_url as
-SELECT b.Prof_ID, c.Logo_URL, if(d.Prof_ID is not null, 1, 0) as cover, count(a.Proj_ID) as count_project, if(c.Logo_URL is not null and d.Prof_ID is not null and count(a.Proj_ID) >= 2,1,0) as Url_Status
+SELECT b.Prof_ID, c.Logo_URL, if(d.Prof_ID is not null, 1, 0) as cover, count(a.Proj_ID) as count_project, if(c.Logo_URL is not null and d.Prof_ID is not null and count(a.Proj_ID) >= 1,1,0) as Url_Status
 from proj_prof_relationship a
 join prof_expertise_relationship b on a.Prof_Expertise_Relationship_ID = b.ID and b.Relationship_Status = '1'
 join professionals c on b.Prof_ID = c.ID and c.Prof_Status = '1'
