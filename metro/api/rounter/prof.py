4e2017b925522afd95893c1cc26febb7c280fa3b
tarr21 <warintorn.realist@gmail.com>
2026-05-20 10:52:29 +0700

    delete cover before upload new

--- metro/api/rounter/prof.py
+++ metro/api/rounter/prof.py
@@ -497,6 +497,7 @@ async def upload_and_record_cover(
         file_bytes = await file.read()
         if ext not in ALLOWED_EXT:
             raise HTTPException(status_code=400, detail=f"File type not allowed: {ext}")
+        _delete_cover(cur, Prof_ID, cover_size_list[0]["ratio"], "prof")
         for cover_size in cover_size_list:
             size = cover_size["size"]
             ratio = str(cover_size["ratio"])
@@ -532,7 +533,7 @@ async def delete_cover_record(
     _ = Depends(get_current_user),
 ):
     conn = get_db()
-    cur = conn.cursor()
+    cur = conn.cursor(dictionary=True)
     try:
         _delete_cover(cur, Prof_ID, Cover_Ratio, "prof")
         conn.commit()
